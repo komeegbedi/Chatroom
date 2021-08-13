d8606af70a345da1261dd2ce36cc4441c888f505
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-08-12 19:18:50 -0500

    updated dark mode to change based on OS

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const checkBox = document.getElementById("switch");
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
+const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches;
+const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
 
-if (prefersDarkScheme.matches) {
-    document.body.classList.add("dark-mode");
+
+const activateDarkMode = () => {
+    document.body.classList.add('dark-mode');
     checkBox.checked = true;
-} 
+}
 
+const activateLightMode = () => {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        checkBox.checked = false;
+    }
+}
+
+
+
+if (isDarkMode) {
+    activateDarkMode();
+}
+if (isLightMode) {
+    activateLightMode();
+}
+
+if (isNotSpecified || hasNoSupport) {
+
+    console.log('You specified no preference for a color scheme or your browser does not support it. I schedule dark mode during night time.')
+    now = new Date();
+    hour = now.getHours();
+    if (hour < 4 || hour >= 16) {
+        activateDarkMode();
+    }
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => e.matches && activateDarkMode());
+window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", e => e.matches && activateLightMode());
 checkBox.addEventListener('change', function () {
     document.body.classList.toggle('dark-mode');
 });
-
 
 

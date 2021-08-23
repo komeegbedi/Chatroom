48f8d7ddc2afe5209dd21b84f0a7b78e046a761d
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-08-22 21:14:58 -0500

    added firebase connection

---
+++ chat.js
@@ -0,0 +1,9 @@
+// a chat is going to contain who sent the message , what room it was sent from , the message sent , the time it was sent
+class Chat{
+
+    constructor(user , room ){
+        this.username = user;
+        this.chatroom = room;
+        
+    }
+}

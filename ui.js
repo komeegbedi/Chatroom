b8e3a7d2ab2fe3c014644651b1c6a32d6b3dc2d7
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-09-02 21:37:50 -0500

    changed the typing timeout to 3.5 secs

--- ui.js
+++ ui.js
@@ -266,7 +266,7 @@ const isTyping = () => {
                     users.doc(userID).update({ isTyping: false });
                     timeOut = undefined;
                     userIsTyping = false;
-                }, 5000);
+                }, 3500);
             }
             else{
             
@@ -277,7 +277,7 @@ const isTyping = () => {
                     timeOut = undefined;
                     userIsTyping = false;
 
-                }, 5000);
+                }, 3500);
             }
         }
         else if (!chatForm.querySelector('button').hasAttribute('disabled')) {
